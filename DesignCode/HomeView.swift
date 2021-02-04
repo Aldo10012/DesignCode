9fdd17cbfd2042d8c6d28441bb6926082e8b98bc
Alberto <aldo10012@gmail.com>
2021-02-04 15:46:29 -0800

    ScrollView and Repeat

--- DesignCode/HomeView.swift
+++ DesignCode/HomeView.swift
@@ -23,6 +23,19 @@ struct HomeView: View {
             .padding(.horizontal)
             .padding(.top, 30)
             
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 30) {
+                    ForEach(0 ..< 5) { item in
+                        SectionView()
+                    }
+                }
+                .padding(20)
+                .padding(.bottom, 30)
+            }
+            
+            .edgesIgnoringSafeArea(.all)
+            
+            
             Spacer()
         }
     }
@@ -31,5 +44,34 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView(showProfile: .constant(false))
+    }
+}
+
+struct SectionView: View {
+    var body: some View {
+        VStack(spacing:16) {
+            HStack(alignment: .top) {
+                Text("Prototype Designs in SwiftUI")
+                    .font(.system(size: 24, weight: .bold))
+                    .frame(width: 160, alignment: .leading)
+                    .foregroundColor(.white)
+                Spacer()
+                Image("Logo1")
+            }
+            
+            Text("18 sections".uppercased())
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 210)
+        }
+        .padding(.top,20)
+        .padding(.horizontal,20)
+        .frame(width: 275, height: 275)
+        .background(Color("card1"))
+        .cornerRadius(30)
+        .shadow(color: Color("card1"), radius: 10, x: 5, y: 10)
     }
 }
